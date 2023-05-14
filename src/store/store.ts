bab82aac92950142334e0576c7ef26d7b7268882
1Molot <sinkevich1990@gmail.com>
2023-05-14 14:59:48 +0300

    fix:fix

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,16 +1,18 @@
-adimport {combineReducers, legacy_createStore as createStore} from "redux";
+import {combineReducers} from "redux";
+
+import { legacy_createStore as createStore} from "redux";
 import {shopListReducer} from "../reducer/shopListReducer";
 import {ThemeReducer} from "../reducer/ThemeReducer";
 
 
-const RootReducer=combineReducers({
-    shopList:shopListReducer,
-    theme:ThemeReducer
+const RootReducer = combineReducers({
+    shopList: shopListReducer,
+    theme: ThemeReducer
 })
 
-export const store=createStore(RootReducer)
-export type RootState=ReturnType<typeof RootReducer>
+export const store = createStore(RootReducer)
+export type RootState = ReturnType<typeof RootReducer>
 
 
 // @ts-ignore
-window.store=store
+window.store = store
